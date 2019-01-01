c0f1c5455b8ca3c51f5fddcadd868eff3fab7a88
nullthefirst <jrandangana@gmail.com>
2019-01-01 16:13:27 +0100

    new year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ export default class Footer extends Component {
   render() {
     return(
       <div style={footerStyle}>
-        <h5>Lead City Fashion & Lifestyle Academy &copy; 2018 | All Rights Reserved</h5>
+        <h5>Lead City Fashion & Lifestyle Academy &copy; 2018 - 2019 | All Rights Reserved</h5>
         <h6 style={underline}><a href="https://about.me/usheninte">Coded by Usheninte</a></h6>
       </div>
     );
